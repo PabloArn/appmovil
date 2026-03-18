c78fa2d2a3a14bfe793186618f20ee4045a292ae
PabloArn <arnaudpablo53@gmail.com>
2026-03-17 21:11:09 -0600

    Agregación de dependencias necesarias / Creación de modelos de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
--- app/src/main/java/com/example/myday/Models.kt
+++ app/src/main/java/com/example/myday/Models.kt
@@ -1,2 +1,24 @@
 package com.example.myday
 
+import android.net.Uri
+
+data class DiaryYear(
+    val id: Int,
+    val title: String, //Título del año, aquí agregamos el nombre de nuestra primera "carpeta" que sería el año
+    val coverImageUri: Uri? = null //Usamos Uri reales que llamamos gracias a las librerías de Coil
+)
+
+data class DiaryMonth(
+    val id: Int,
+    val yearId: Int,
+    val title: String, //Título del mes, aquí agregamos el nombre de nuestra segunda "carpeta" que sería el mes
+    val chronologicalOrder: Int, //Agrega el orden a la lista de los meses, enero -> 1, febrero -> 2, etc.
+    val coverImageUri: Uri? = null
+)
+
+data class DiaryEntry(
+    val id: Int,
+    val monthId: Int,
+    val titleText: String, //Título del día, aquí agregamos el nombre de nuestra tercera "carpeta" que sería el día
+    val entryImageUri: Uri? = null
+)
